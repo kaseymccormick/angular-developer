69a5c2591f887f31c72cc941de2c6b46db03b49c
Kasey McCormick <kasey.mccormick@atssecured.com>
2019-02-06 22:52:42 -0600

    linting changes

--- src/app/@core/auth/interceptor.ts
+++ src/app/@core/auth/interceptor.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Observable }  from 'rxjs';
+import { Observable } from 'rxjs';
 
 
 @Injectable()
 export class MyInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const https = req.clone({
-      url: req.url.replace('http://', 'https://')
+      url: req.url.replace('http://', 'https://'),
     });
     return next.handle(https);
   }
